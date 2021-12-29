527d8dc4e1a5de02d6ee85811bfb027d5db1a72d
Irawan <irawan@live.com.au>
2021-12-29 23:32:51 +0930

    UC4 the component

---
+++ src/Application.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Application extends Component {
+
+	render() {
+
+		return (
+			<div>
+				<h1>Nasi </h1> 
+				<h2>campur</h2>
+			</div>
+			);
+		
+	}
+}
+
+export default Application;
