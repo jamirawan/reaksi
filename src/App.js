11bf53113bd7076e531ae80b8509cab518855579
Irawan <irawan@live.com.au>
2022-12-20 09:32:00 +0800

    added components and imported

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import Nav from './components/Nav.js';
+import Promo from './components/Promo.js';
+import Intro1 from './components/Intro1.js';
+import Footer from './components/Footer.js';
+import Intro2 from './components/Intro2.js';
+import Intro3 from './components/Intro3.js';
+import Heading from './components/Heading.js';
+
+
 import './App.css';
 
-function App() {
+
+
+ function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Heading />
+    <Nav />
+    <Promo />
+    <Intro1 />
+    <Intro2 />
+    <Intro3 />
 
-export default App;
+
+    </div>
+  
+    );
+ }
+
+ export default App;
---
+++ src/components/Heading.js
@@ -0,0 +1,9 @@
+function Heading () {
+  return (
+    <div className="heading-page">
+      <h1>Irawan REACT's page </h1>
+    </div>
+    );
+};
+
+export default Heading;
