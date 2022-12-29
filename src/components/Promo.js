470a8ca7cd3003303b079e4b014c683c511c4ae1
Irawan <irawan@live.com.au>
2022-12-29 15:00:04 +0800

    who

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -2,10 +2,10 @@ function Promo() {
     return (
         <div className="promo-section">
             <div>
-                <h1>Don't miss this deal!</h1>
+                <h2>Don't miss this deal!</h2>
                 </div>
                 <div>
-                <h2>Subscribe to my newsletter and get all the shop items at 50% off!</h2>
+                <h2>Who</h2>
             </div>
         </div>
     );
