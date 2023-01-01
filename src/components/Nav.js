76a73f33479903ebb940076000803522f5476690
Irawan <irawan@live.com.au>
2023-01-01 16:37:09 +0800

    added portfolio to the menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -42,6 +42,9 @@ export default function Navbar() {
             <a href="/about">About</a>
           </li>
           <li>
+            <a href="/portfolio">Portfolio</a>
+          </li>
+          <li>
             <a href="/contact">Contact</a>
           </li>
         </ul>
